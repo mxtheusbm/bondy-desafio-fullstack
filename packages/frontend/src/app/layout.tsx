59c370312d7777eb0e500e8e88f94976fcc46558
Matheus Barbosa <mxtheusbm@gmail.com>
2025-08-18 22:47:30 -0300

    chore: call client in provider

--- packages/frontend/src/app/layout.tsx
+++ packages/frontend/src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { Toaster } from "@/components/ui/sonner";
 import { ApolloProvider } from "@apollo/client";
+import { client } from "@/lib/apollo-client";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -29,7 +30,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <ApolloProvider>
+        <ApolloProvider client={client}>
           {children}
           <Toaster />
         </ApolloProvider>
